060e7bfeca68ecebd6177931bd625ae9a65c8c49
Priyanka <padhiyarmahi281@gmail.com>
2025-07-03 22:11:06 +0530

    Initial commit for Krishna Clinic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import { LanguageProvider } from './context/LanguageContext';
 import Header from './components/Header';
 import Home from './pages/Home';
@@ -9,16 +10,16 @@ import Contact from './pages/Contact';
 
 function App() {
   return (
-    <>
-    <LanguageProvider>
-      <Header />
-      <Home />
-      <About />
-      <Services />
-      <Appointment />
-      <Contact />
+    <BrowserRouter basename="/krishna-clinic">
+      <LanguageProvider>
+        <Header />
+        <Home />
+        <About />
+        <Services />
+        <Appointment />
+        <Contact />
       </LanguageProvider>
-    </>
+    </BrowserRouter>
   );
 }
 
